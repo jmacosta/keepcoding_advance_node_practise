f44dd53affef7741712cd8d863aac9807dc99ecc
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2023-10-05 22:51:52 +0200

    Create Model product

---
+++ Models/Product.js
@@ -0,0 +1,9 @@
+export class ProductModel {
+  // to do bd statics methods
+  static async getAll() {}
+  static async getTags() {}
+  static async create() {}
+  // not required
+  static async update() {}
+  static async delete() {}
+}
