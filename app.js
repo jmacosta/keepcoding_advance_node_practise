3218c233ffa399a56783e00420cd4948355d02ae
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2023-10-05 19:48:00 +0200

    Server listening

--- app.js
+++ app.js
@@ -4,3 +4,7 @@ app.use(json());
 app.get('/', (req, res) => {
   res.json({ message: 'Wellcome to Nodepop' });
 });
+const PORT = process.env.PORT ?? 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port http://localhost:${PORT}`);
+});
