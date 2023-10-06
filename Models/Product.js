6397091310095e520eb848a2408dfa7bd55c7e19
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2023-10-06 21:29:58 +0200

    Create methods for  model

--- Models/Product.js
+++ Models/Product.js
@@ -1,9 +1,21 @@
+import { randomUUID } from 'node:crypto';
+import { readJson } from '../utils.js';
+const products = readJson('./data/products.json');
+const productsWithId = products.map(product => {
+  return { id: randomUUID(), ...product };
+});
 export class ProductModel {
   // to do bd statics methods
-  static async getAll() {}
+  static async getAll() {
+    return productsWithId;
+  }
   static async getTags() {}
   static async create() {}
   // not required
   static async update() {}
   static async delete() {}
+  static async getById({ id }) {
+    const product = productsWithId.find(product => product.id === id);
+    return product;
+  }
 }
