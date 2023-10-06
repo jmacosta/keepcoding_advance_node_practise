41557d4663ca9657246abf5d14ea0dcc8b3d5ab5
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2023-10-06 21:43:37 +0200

    add uuid to schema

--- Schemas/products.js
+++ Schemas/products.js
@@ -1,11 +1,14 @@
 import mongose from 'mongoose';
+import { v4 as uuidv4 } from 'mongoose-uuid';
+
 export const productSchema = mongose.Schema(
   {
+    id: { type: String, unique: true, default: uuidv4 },
     name: { type: String, index: true },
     sellOrSearch: { type: String, index: true },
     price: { type: String },
     image: { type: String },
-    tags: [{ type: String, index: true }]
+    tags: [{ type: String }]
   },
   { collection: 'products' }
 );
