eb9da8aa6374604e5d1e0e4ffe7a116d94f71a50
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2023-10-06 21:44:44 +0200

    add connect products router

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import express, { json } from 'express';
+import { productsRouter } from './routes/products.js';
 const app = express();
 app.use(json());
 app.get('/', (req, res) => {
   res.json({ message: 'Wellcome to Nodepop' });
 });
+
+app.use('/products', productsRouter);
 const PORT = process.env.PORT ?? 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on port http://localhost:${PORT}`);
