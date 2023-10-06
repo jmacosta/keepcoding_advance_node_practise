3d8ff49b95f347457208597cdcc20082559e7d63
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2023-10-06 21:47:03 +0200

    products routes

--- routes/products.js
+++ routes/products.js
@@ -1,2 +1,11 @@
 import { Router } from 'express';
+import { productController } from '../controllers/products.js';
 export const productsRouter = Router();
+productsRouter.get('/', productController.getAll);
+productsRouter.post('/', productController.create);
+
+// to do
+
+productsRouter.get('/:id', productController.getById);
+productsRouter.delete('/:id', productController.delete);
+productsRouter.patch('/:id', productController.patch);
