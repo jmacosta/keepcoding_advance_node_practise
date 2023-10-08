5259781a50d6e9a18b2dca87e67e82cc7ab21f00
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2023-10-08 13:27:13 +0200

    Connect with Mongo DB

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 import express, { json } from 'express';
+import { connectDB } from './db/connectMongoose.js';
 import { productsRouter } from './routes/products.js';
 const app = express();
 app.use(json());
+await connectDB();
 app.get('/', (req, res) => {
   res.json({ message: 'Wellcome to Nodepop' });
 });
 
 app.use('/products', productsRouter);
+
 const PORT = process.env.PORT ?? 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on port http://localhost:${PORT}`);
