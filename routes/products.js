68f17493223378a4c700bfdb36c2309cd4c0e13f
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2023-12-20 18:41:49 +0100

    feat: refactor routes to remove api routes

--- routes/products.js
+++ routes/products.js
@@ -2,11 +2,5 @@ import { Router } from 'express';
 import { productController } from '../controllers/products.js';
 export const productsRouter = Router();
 productsRouter.get('/', productController.getAll);
-productsRouter.post('/', productController.create);
-productsRouter.get('/tags', productController.getTags);
 
 // not required
-
-productsRouter.get('/:id', productController.getById);
-productsRouter.delete('/:id', productController.delete);
-productsRouter.patch('/:id', productController.patch);
