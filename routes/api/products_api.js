a6dd8582ca76037b1f7c383708d10d93b4a7476f
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-01-01 14:01:16 +0100

    feat: add login route to product api

--- routes/api/products_api.js
+++ routes/api/products_api.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
+import { LoginController } from '../../controllers/LoginController.js';
 import { ProductApiController } from '../../controllers/api/productApiController.js';
 const productApiController = new ProductApiController();
+const loginController = new LoginController();
 export const productsApiRouter = Router();
 productsApiRouter.get('/', productApiController.getAll);
 productsApiRouter.post('/', productApiController.create);
 productsApiRouter.get('/tags', productApiController.getTags);
+productsApiRouter.post('/authenticate', loginController.post);
 
 // not required
 
