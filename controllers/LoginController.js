4d0013cd4b81ec528ffdad614ec3a12793ce8679
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-01-01 19:40:34 +0100

    Feat: add session init

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -3,7 +3,7 @@ export class LoginController {
   index(req, res, next) {
     res.locals.title = 'Nodepop';
     res.locals.error = '';
-    res.local.email = '';
+    res.locals.email = '';
     res.render('login');
   }
   async post(req, res, next) {
@@ -17,10 +17,10 @@ export class LoginController {
         res.locals.title = 'Nodepop';
         res.locals.error = 'Invalid credentials';
         res.locals.email = email;
-
         res.render('login');
         return;
       }
+      req.session.userLogged = user._id;
       res.redirect('/products');
     } catch (error) {
       next(error);
