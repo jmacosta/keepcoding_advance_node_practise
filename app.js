d6ac990706fc1fd61dc89145182a69e929be7d9f
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-01-01 13:58:37 +0100

    Feat : add logger, cookie parser and refactor middlewares for form use

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+import cookieParser from 'cookie-parser';
 import express, { json } from 'express';
+import logger from 'morgan';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { connectDB } from './db/connectMongoose.js';
 import { productsApiRouter } from './routes/api/products_api.js';
+import { loginRouter } from './routes/login.js';
 import { productsRouter } from './routes/products.js';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const app = express();
@@ -15,9 +19,13 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
   res.json({ message: 'Wellcome to Nodepop' });
 });
-
+// middlewares
+app.use(logger('dev'));
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
 app.use('/products', productsRouter);
 app.use('/api/', productsApiRouter);
+app.use('/login', loginRouter);
 app.use((req, res, next) => {
   res.status(404).json({ error: 'path not found' });
 });
