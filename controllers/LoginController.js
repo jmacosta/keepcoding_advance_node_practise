18a15cc46d8239a71f48b505181104c940ea75af
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-01-07 15:33:54 +0100

    FIX: Refactor for translate error message

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -1,3 +1,4 @@
+import jwt from 'jsonwebtoken';
 import { User } from '../Models/User.js';
 export class LoginController {
   index(req, res, next) {
@@ -11,16 +12,41 @@ export class LoginController {
       const { email, password } = req.body;
       const user = await userDB.findUser({ email });
       if (!user || !(await userDB.comparePasswords(password, user.password))) {
-        //TODO: refactor to use i18n
-        //res.locals.error = req.__('Invalid credentials');
-
-        res.locals.error = 'Invalid credentials';
+        res.locals.error = req.__('Invalid credentials');
         res.locals.email = email;
         res.render('login');
         return;
       }
       req.session.userLogged = user._id;
       res.redirect('/');
+    } catch (error) {
+      next(error);
+    }
+  }
+  logout(req, res, next) {
+    req.session.regenerate(err => {
+      if (err) {
+        next(err);
+        return;
+      }
+      res.redirect('/');
+    });
+  }
+  async postJWT(req, res, next) {
+    try {
+      const userDB = new User();
+      const { email, password } = req.body;
+      const user = await userDB.findUser({ email });
+      if (!user || !(await userDB.comparePasswords(password, user.password))) {
+        res.json({ error: 'Invalid credentials' });
+        return;
+      }
+      const tokenJWT = await jwt.sign(
+        { _id: user._id },
+        process.env.JWT_SECRET,
+        { expiresIn: '2h' }
+      );
+      res.json({ jwt: tokenJWT });
     } catch (error) {
       next(error);
     }
