523beceb5942b38bb132df58ab9cb4b197464a73
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-01-08 14:13:51 +0100

    Feat Add multer middleware

--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,18 @@
 import { Router } from 'express';
+import multer from 'multer';
 import { LoginController } from '../controllers/LoginController.js';
 import { ProductsController } from '../controllers/productsController.js';
 import sessionAuthMiddleware from '../lib/sessionAuthMiddleware.js';
 const productsController = new ProductsController();
 const loginController = new LoginController();
+const storage = multer.diskStorage({
+  destination: './public/assets/',
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + '-' + uniqueSuffix + file.originalname);
+  }
+});
+const upload = multer({ storage: storage }).single('image');
 export const productsRouter = Router();
 productsRouter.get('/', sessionAuthMiddleware, productsController.getAll);
 productsRouter.get(
@@ -14,6 +23,7 @@ productsRouter.get(
 productsRouter.post(
   '/new-product',
   sessionAuthMiddleware,
+  upload,
   productsController.create
 );
 
