04409b21adb59801545deb82325b812910305d6f
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-01-12 22:10:20 +0100

    FEAT: add multer middleware

--- routes/api/products_api.js
+++ routes/api/products_api.js
@@ -1,13 +1,22 @@
 import { Router } from 'express';
+import multer from 'multer';
 import { LoginController } from '../../controllers/LoginController.js';
 import { ProductApiController } from '../../controllers/api/productApiController.js';
+const storage = multer.diskStorage({
+  destination: './public/assets/',
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + file.originalname;
+    cb(null, uniqueSuffix);
+  }
+});
+const upload = multer({ storage: storage }).single('image');
 const productApiController = new ProductApiController();
 const loginController = new LoginController();
 export const productsApiRouter = Router();
 productsApiRouter.get('/', productApiController.getAll);
-productsApiRouter.post('/', productApiController.create);
+productsApiRouter.post('/', upload, productApiController.create);
 productsApiRouter.get('/tags', productApiController.getTags);
-productsApiRouter.post('/authenticate', loginController.postJWT);
+//productsApiRouter.post('/authenticate', loginController.postJWT);
 
 // not required
 
