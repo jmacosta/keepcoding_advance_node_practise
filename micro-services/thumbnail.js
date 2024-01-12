8f6aec3c3275fda9bb0351e5f35139d0ad390078
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-01-12 18:57:43 +0100

    Feat: add enviroment var for thmbs path

--- micro-services/thumbnail.js
+++ micro-services/thumbnail.js
@@ -1,5 +1,10 @@
 import cote from 'cote';
+import dotenv from 'dotenv';
+import fs from 'node:fs';
+import path from 'node:path';
 import sharp from 'sharp';
+dotenv.config();
+
 const { Responder } = cote;
 const responder = new Responder({ name: 'Thumbnail service' });
 responder.on('create-thumbnail', (req, done) => {
@@ -11,13 +16,28 @@ responder.on('create-thumbnail', (req, done) => {
 
 const resizeImage = async (image, origin) => {
   const originalImage = origin + image;
+  const destFolder = checkDestFolder();
   sharp(originalImage)
     .resize({ height: 100 })
-    .toFile(`./public/thumbs/thumbnail-${image}`, (err, info) => {
+    .toFile(`${destFolder}thumbnail-${image}`, (err, info) => {
       if (err) {
         console.log('error processing image', err);
       } else {
         console.log('image processed successfully', info);
       }
     });
+};
+
+const checkDestFolder = () => {
+  const actualFolder = process.cwd();
+  const thumbsfolder = path.join(actualFolder, process.env.THUMBS_PATH);
+  if (!fs.existsSync(thumbsfolder)) {
+    try {
+      fs.mkdirSync(thumbsfolder, { recursive: true });
+      console.log('Create folder: ', thumbsfolder);
+    } catch (error) {
+      console.error('Error creating folder: ', error);
+    }
+  }
+  return thumbsfolder;
 };
