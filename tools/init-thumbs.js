3df14ea75c6042e4f7f015c32314d05c83173f1b
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-01-12 11:39:49 +0100

    Feat: script for create initial thumbs of db

--- tools/init-thumbs.js
+++ tools/init-thumbs.js
@@ -7,26 +7,31 @@ const assetsFolder = path.join(actualFolder, 'public/assets/');
 const requester = new Requester({ name: 'Image for Thumb' });
 
 const createThumb = (image, origin) => {
-  const event = {
-    type: 'create-thumbnail',
-    origin: origin,
-    image: image
-  };
-  requester.send(event, result => {
-    console.log(Date.now(), 'Message from Service ', result);
+  return new Promise((resolve, reject) => {
+    const event = {
+      type: 'create-thumbnail',
+      origin: origin,
+      image: image
+    };
+    requester.send(event, result => {
+      console.log(Date.now(), 'Message from Service ', result);
+      resolve();
+    });
   });
 };
 
 fs.readdir(assetsFolder, async (err, files) => {
-  const promisesThumbs = [];
   if (err) {
     console.error('Error in the folder', err);
     return;
   }
-  files.forEach(file => {
-    createThumb(file, assetsFolder);
-  });
-
-  console.log('All thumbs created');
-  //process.exit();
+  const promisesThumbs = files.map(file => createThumb(file, assetsFolder));
+  try {
+    await Promise.all(promisesThumbs);
+    console.log('All thumbs created');
+    process.exit();
+  } catch (error) {
+    console.error('Error creating thumbs', error);
+    process.exit(1);
+  }
 });
